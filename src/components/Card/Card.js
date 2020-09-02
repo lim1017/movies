bb8a6876a3dbed55f138a10b0cf423a71e8313d5
tom <lim1017@hotmail.com>
2020-09-01 23:15:27 -0400

    media query with usestyles

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,49 +2,57 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
-import './_Card.sass'
 
-let mainContainerHeight = window.innerWidth < 750 ? "50vh" : "80vh"
+import { useTheme } from '@material-ui/core/styles';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 
 
-const useStyles = makeStyles({
-  root: {
-    minWidth: 275,
-    border: "3px solid black",
-    borderRadius:"5px",
-    boxShadow: "10px 5px 5px red",
-    padding: 0,
-    height: mainContainerHeight,
-    overflow:"scroll"
-  },
-  small:{
-    border: "5px solid black",
-    borderRadius:"5px",
-    boxShadow: "10px 5px 5px grey",
-    marginBottom:"5px",
-    marginTop:"8px",
-    padding:"0px"
-  },
-  nomination:{
-    border: "3px solid black",
-    borderRadius:"5px",
-    boxShadow: "10px 5px 5px red",
-    padding: 0,
-    height:mainContainerHeight,
-    overflow:"scroll"
-  },
-  noPadding:{
-    padding:"0px !important",
+
+const useStyles = makeStyles((theme)=> {
+
+  return {
+    root: {
+      minWidth: 275,
+      border: "3px solid black",
+      borderRadius:"5px",
+      boxShadow: "10px 5px 5px red",
+      padding: 0,
+      height: "50vh",
+      overflow:"scroll",
+      [theme.breakpoints.up(750)]: {
+        height: "80vh"                   // secondary
+      }
+    },
+    small:{
+      border: "5px solid black",
+      borderRadius:"5px",
+      boxShadow: "10px 5px 5px grey",
+      marginBottom:"5px",
+      marginTop:"8px",
+      padding:"0px"
+    },
+    nomination:{
+      border: "3px solid black",
+      borderRadius:"5px",
+      boxShadow: "10px 5px 5px red",
+      padding: 0,
+      height:"50vh",
+      overflow:"scroll",
+      [theme.breakpoints.up(750)]: {
+        height: "80vh"                   // secondary
+      }
+    },
+    noPadding:{
+      padding:"0px !important",
+    }
   }
+
 });
 
 export default function SimpleCard(props) {
   const classes = useStyles();
   const style = props.small ? classes.small : props.nomination ? classes.nomination : classes.root
-
   const padding = props.noPadding ? classes.noPadding : ""
-
-  console.log()
 
   return (
     <Card className={style}>
